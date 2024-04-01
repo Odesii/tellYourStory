84188fa37876268d189726b5ef8d00bde01c410c
Odesii <alic.madd@gmail.com>
2024-03-31 22:40:52 -0400

    filled out all code, and content

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,6 +3,9 @@ const title =  document.querySelector(`#title`)
 const blog = document.querySelector(`#blog`)
 const submitBttn = document.querySelector(`#submit`)
 
+const switchMode = document.querySelector(`#dark`);
+const theme = document.body;
+
 submitBttn.addEventListener('click', function(event) {
     event.preventDefault();
 
@@ -17,5 +20,37 @@ submitBttn.addEventListener('click', function(event) {
     }
     localStorage.setItem(`user`, JSON.stringify(updatePage));
     location.href = `./stories.html`;
+});
+
+let mode = `light`;
+
+switchMode.addEventListener(`click`, function(event){ 
+    event.preventDefault();
+
+
+    if (mode === 'light') {
+        mode = 'dark';
+        theme.setAttribute('class', 'dark-color');
+        localStorage.setItem('mode', 'dark');
+    }
+    
+    else {
+        mode = 'light';
+        theme.setAttribute('class', ``);
+        localStorage.setItem('mode', 'light');
+    }
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const getTheme = localStorage.getItem('mode');
+
+    if (getTheme === 'dark'){
+        theme.setAttribute('class', 'dark-color');
+    }
+    else {
+        theme.setAttribute('class', ``);
+}
 });
 
--- assets/js/story.js
+++ assets/js/story.js
@@ -1,4 +1,6 @@
 const backButton = document.querySelector("#back");
+const switchMode = document.querySelector(`#dark`);
+const theme = document.body;
 
 backButton.addEventListener(`click`, function (event) {
     event.preventDefault();
@@ -9,10 +11,39 @@ function getStory(){
     const getInfo = JSON.parse(localStorage.getItem(`user`));
 
     if (getInfo !== null){
-        document.querySelector(`#handel`).innerHTML = getInfo.t1t13;
-        document.querySelector('#title').setAttribute('data-title', getInfo.h4nD31);
+        document.querySelector('#handel').setAttribute('data-title', getInfo.h4nD31);
+        document.querySelector(`#title`).innerHTML = ('->' + getInfo.t1t13 + '<-');
         document.querySelector(`#story`).innerHTML = getInfo.S7orY;
     }
 }
 
-document.addEventListener('DOMContentLoaded', getStory);
+document.addEventListener('mousemove', getStory);
+document.addEventListener('DOMContentLoaded', function(){
+    const getTheme = localStorage.getItem('mode');
+
+    if (getTheme === 'dark'){
+        theme.setAttribute('class', 'dark-color');
+    }
+    else {
+        theme.setAttribute('class', ``);
+}
+});
+
+
+let mode = `light`;
+
+switchMode.addEventListener(`click`, function(event){ 
+    event.preventDefault();
+
+
+    if (mode === 'light') {
+        mode = 'dark';
+        theme.setAttribute('class', 'dark-color');
+    }
+    
+    else {
+        mode = 'light';
+        theme.setAttribute('class', ``);
+    }
+
+});
