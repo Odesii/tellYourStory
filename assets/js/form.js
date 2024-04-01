5dbc3d35feced2b7f14144c29a409ea413ffde35
Odesii <alic.madd@gmail.com>
2024-03-31 23:09:16 -0400

    added comments to code for better understanding

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,29 +5,33 @@ const submitBttn = document.querySelector(`#submit`)
 
 const switchMode = document.querySelector(`#dark`);
 const theme = document.body;
-
+//  on clicking submit button, the user input is stored in local storage and take you to the  page
 submitBttn.addEventListener('click', function(event) {
     event.preventDefault();
-
+//  this is the object that will be stored in local storage
     const updatePage = {
         h4nD31: handel.value,
         t1t13: title.value,
         S7orY: blog.value,
     };
+//  if the user does not input anything, an alert will pop up
     if(!updatePage.h4nD31 || !updatePage.t1t13 || !updatePage.S7orY) {
         alert('can you even triforce?');
         return;
     }
+//  this is the key that will be used to store the object in local storage
     localStorage.setItem(`user`, JSON.stringify(updatePage));
     location.href = `./stories.html`;
 });
-
+// START OF DARK MODE
 let mode = `light`;
-
+// looks for button click and changes the theme to dark or light
 switchMode.addEventListener(`click`, function(event){ 
     event.preventDefault();
 
-
+// if light mode is on, it will change to dark mode
+// and set class to dark-color in 'body' element\
+// and set local storage to dark
     if (mode === 'light') {
         mode = 'dark';
         theme.setAttribute('class', 'dark-color');
@@ -42,7 +46,8 @@ switchMode.addEventListener(`click`, function(event){
 
 });
 
-
+// reads local storage and sets the theme to dark if it is dark
+// keeps theme consistent when user navigates to different pages
 document.addEventListener('DOMContentLoaded', function(){
     const getTheme = localStorage.getItem('mode');
 
--- assets/js/story.js
+++ assets/js/story.js
@@ -1,23 +1,25 @@
 const backButton = document.querySelector("#back");
 const switchMode = document.querySelector(`#dark`);
 const theme = document.body;
-
+//sends user back to the form page
 backButton.addEventListener(`click`, function (event) {
     event.preventDefault();
     location.href = `/index.html`;
 });
-
+//gets the user input from local storage and displays it on the page
 function getStory(){
     const getInfo = JSON.parse(localStorage.getItem(`user`));
-
+//  if the user input is not null, it will display the user input on the page
+// by changing the innerHTML of the elements and the data-title of an element
     if (getInfo !== null){
         document.querySelector('#handel').setAttribute('data-title', getInfo.h4nD31);
         document.querySelector(`#title`).innerHTML = ('->' + getInfo.t1t13 + '<-');
         document.querySelector(`#story`).innerHTML = getInfo.S7orY;
     }
 }
-
-document.addEventListener('mousemove', getStory);
+//calls the function to get the user input and adds that to page when page loads 
+document.addEventListener('DomContentLoaded', getStory);
+// reads local storage and sets the theme to dark if it is dark
 document.addEventListener('DOMContentLoaded', function(){
     const getTheme = localStorage.getItem('mode');
 
@@ -29,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function(){
 }
 });
 
-
+// START OF DARK MODE
 let mode = `light`;
 
 switchMode.addEventListener(`click`, function(event){ 
